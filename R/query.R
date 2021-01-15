37aaeaedbdbd77c76cfe48df7e7837b718944ba1
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-01-14 20:34:14 -0500

    Update query.R

--- R/query.R
+++ R/query.R
@@ -1,7 +1,14 @@
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- #
 # Name: query.R
-# Description: queries the main database to create working datasets
+# Description: queries the main database to save working datasets
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- #
+
+
+# toggles --------------------------------------------------------------
+
+q1 = 1 # Years 2018-2020
+q2 = 1 # Year 2020 
+
 
 
 # load main dataset ----------------------------------------------------
@@ -9,18 +16,19 @@ bks <- readRDS(file.path(processed, "data/bks-full.Rda"))
 
 
 
-
+# queries ==============================================================
 # query 1: most recent 3 years
-bks_3yr <- filter(bks, year >= 2018)
+
+if (q1 == 1) {
+  bks_3yr <- filter(bks, year >= 2018)
+  saveRDS(bks_3yr, 
+          file.path(processed, "data/years/bks_2018-20.Rda"))
+}
 
 
 # query 2: most recent 1 year 
-bks_3yr <- filter(bks, year == 2020)
-
-
-# export 
-saveRDS(bks_3yr, 
-        file.path(processed, "data/years/bks_2018-20.Rda"))
-
-saveRDS(bks_1yr, 
-        file.path(processed, "data/years/bks_2020.Rda"))
+if (q2 == 2) {
+  bks_1yr <- filter(bks, year == 2020)
+  saveRDS(bks_1yr, 
+          file.path(processed, "data/years/bks_2020.Rda"))
+}
