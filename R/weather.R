5ebd8c64a24880afb774dfcfa155181dde0af948
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-01-26 14:47:04 -0500

    working version of weather function

--- R/weather.R
+++ R/weather.R
@@ -2,82 +2,88 @@
 # extracts info from NOAA databases to create by-hour/day dictionary of basic weather info
 
 # key variables: date/time, temp, precipitation
-
+library(tidyverse)
 library(rnoaa)
 library(lubridate)
-library(sf)
-library(mapview)
 
-# determine locations ------------------------------------------------------------------------------------
-ncdc_locs_cats() # location categories
-ncdc_locs(locationcategoryid = c('ST'), sortfield = 'name', limit = 100) # search by category: arlington CITY:US510002
-va_loc <- ncdc_stations(locationid = 'CITY:US510002', limit = 100)  # find stations that meet location criteria
-uk_loc <- ncdc_stations(locationid = 'CITY:UK000005', limit = 100)
-ca_loc<- ncdc_stations(loc = 'CITY:US060033', limit = 100)
-
-# filter and amend data for sorting and plotting
-va_filter <- va_loc$data %>%
-  mutate(
-    min = as_date(mindate),
-    max = as_date(maxdate),
-    min_yr= year(min),
-    max_yr= year(max)
-  ) %>%
-  filter(min_yr <= 2010, max_yr >=2020) %>%
-  st_as_sf(coords=c("longitude", "latitude")) 
-
-
-# key findings:
-# stationid: 
-#     GHCND:US1MDMG0042 is good for now (takoma park)
-#     
-# locationid: 
-#     CITY:US510002
-
-
-# sample california workflow ===============================================================
-
-# 1. find station -------------------------------------------------------------------------------
-# find the way to search for a station number of what i want
-ncdc_locs_cats() # location categories
-
-# then after I know that I'm looking for states, search for it
-ncdc_locs(locationcategoryid = c('ST'), sortfield = 'name', limit = 100) # ca is FIPS:06
-
-
-# determine a data category, sort by state of ca (grouping of datatypes)
-datacats <- ncdc_datacats(locationid = "FIPS:06", limit = 300) # result: air temperature (TEMP)
-
-
-# could further refine by datatype: filter by location and datacat
-datatype <- ncdc_datatypes(locationid = 'FIPS:06', datacategoryid = 'TEMP', limit = 300) # air temp mean (HLY-TEMP-NORMAL)
-
-
-# look for weather stations in in locationid, datacat, datatype
-stations <- ncdc_stations(locationid = 'FIPS:06', datacategoryid = 'TEMP', limit = 300) # big sur: COOP:040790
+# A new search ====================================================================================================
+# documentation: https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt
+# hourly precip: 
+# daily temp: Daily summaries (GHCN-Daily), DSI 9101_01, c00681
+# datacats <- ncdc_datacats(datasetid = 'GHCND') %>% .$data # temp
+# temp_types <- ncdc_datatypes(datasetid = 'GHCND', datacategoryid = 'TEMP') %>% .$data
+# stations <- ncdc_stations(datasetid = 'GHCND', locationid = 'FIPS:11', datatypeid = 'TAVG') %>% .$data
+# datasets <- ncdc_datasets(datasetid = 'GHCND', datatypeid = 'TAVG', stationid = 'GHCND:USC00186350', startdate = '2010-01-01', enddate = '2010-02-01', limit = 100) %>% .$data
+# data <- ncdc(datasetid = 'GHCND',
+#             datatypeid = c('TMAX', 'PRCP'),
+#              stationid = 'GHCND:USC00186350',
+#              startdate = '2010-01-01',
+#              enddate = '2010-12-31',
+#              limit = 1000) %>%
+#   .$data # note, values given in tenths, degrees C (22.1 = 2.21 Celcius), PRCP in tenths of mm (22 = 2.2 mm)
+# 
 
 
 
+# function for extracting data for a certain year =================================================================
+daily_weather <- function(x) {
+  
+  data <- ncdc(datasetid = 'GHCND',
+               datatypeid = c('TMAX', 'PRCP'),
+               stationid = 'GHCND:USC00186350',
+               startdate = paste0(x, '-01-01'),
+               enddate = paste0(x, '-12-31'),
+               limit = 1000) %>% .$data
+    
+  # generate variables
+  # includes: more computer-readable date-time variables and human-readable measurements.
+  # note that since measurements of temp are recorded in tenth of degrees celsius and precip
+  # in tenth of mm, I will convert to degrees celcius and mm accordingly. First I'll pivot
+  # to wide format so temp and precip are in different columsn
+  data <-
+    data %>% 
+      pivot_wider(names_from = datatype, values_from = value) %>%
+    mutate(   
+      datetime = ymd_hms(date, tz = 'America/New_York'),
+      year     = as.integer(year(datetime)),
+      month    = as.integer(month(datetime)),
+      day      = as.integer(day(datetime)),
+      tempmax  = round((TMAX/10), 1), # daily tempmax in celcius
+      precip   = round((PRCP/10), 1) # daily precip in mm
+    ) 
+  
+  data
+    
+}
 
-# 2. find out data that's available at the station --------------------------------------------
+# extract weather for each year ===================================================================================
 
-# search by datacat
-station.cat <- ncdc_datacats(stationid = 'COOP:040790')
+weather2010 <- daily_weather(2010)
+weather2011 <- daily_weather(2011)
+weather2012 <- daily_weather(2012)
+weather2013 <- daily_weather(2013)
+weather2014 <- daily_weather(2014)
+
+weather2015 <- daily_weather(2015)
+weather2016 <- daily_weather(2016)
+weather2017 <- daily_weather(2017)
+weather2018 <- daily_weather(2018)
+weather2019 <- daily_weather(2019)
+
+weather2020 <- daily_weather(2020)
 
 
-# search by datatype (more refined)
-station.type <- ncdc_datatypes(stationid = 'COOP:040790') # precipitation HPCP (but there are many precip indicators)
+# append 
+weather_daily <- 
+  bind_rows(
+  weather2010, weather2011, weather2012, weather2013, weather2014,
+  weather2015, weather2016, weather2017, weather2018, weather2019,
+  weather2020
+)
 
-
-# search by dataset (and any combination of the datacats or types above)
-station.sets <- ncdc_datasets(stationid = 'COOP:040790', datatypeid = 'HPCP') # PRECIP_HLY (datasetid)
-
-
-# 3. Get data with dataset id ----------------------------------------------------------
-bigsur.precip <- ncdc(datasetid = 'PRECIP_HLY',
-                      stationid = 'COOP:040790',
-                      startdate = '2012-01-01',
-                      enddate = '2012-05-01',
-                      limit = 50)
-view(bigsur.precip$data)
+# export 
+saveRDS(
+  weather_daily,
+  file = file.path(processed, "data/weather/weather-daily.Rda")
+)
 
