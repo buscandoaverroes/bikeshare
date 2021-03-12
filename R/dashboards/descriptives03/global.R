d85ee8e5a4e9c04635c980b82218ec029b5ba54d
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-03-12 12:30:52 -0500

    add global script

---
+++ R/dashboards/descriptives03/global.R
@@ -0,0 +1,4 @@
+# global.R
+# defines objects available to both UI and SERVER
+
+# color palettes ---------------------------------------------------------------------------------------------
--- R/main-bks.R
+++ R/main-bks.R
@@ -86,7 +86,7 @@
   s2 <- 0   # stations      creates old/new station number dictionary and adds station features
             #                   makes: station_key.Rda, station-geo-objects.Rdata
                             
-  s3 <- 0   # construct:      takes bks.Rda and makes other files, runs station-number.R
+  s3 <- 1   # construct:      takes bks.Rda and makes other files, runs station-number.R
             #                   makes: bks-full.Rda, bks-full.csv
   s4 <- 0   # query:          filters/queries main database and exports files.
             #                   makes: bks_2020.Rda, bks1720.Rda
@@ -96,7 +96,7 @@
             #                   makes: stats10-14.Rdata ~20 min
   s6 <- 0   # stats15-16       takes years 15-16 from query, processes, adds station info, stats.
             #                   makes: stats15-16.Rdata ~20 min
-  s7 <- 1   #stats17-20        takes years 17-20 from query, processes, adds station info, stats.
+  s7 <- 0   #stats17-20        takes years 17-20 from query, processes, adds station info, stats.
             #                    makes: stats17-20.Rdata ~20 min
   
 # recollection 
