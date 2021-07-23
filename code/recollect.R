c3f2345ae490ce8414336c7f75642ae4fd2084af
Tom <buscandoaverroes@icloud.com>
2021-07-22 20:03:57 -0400

    update n_rides rdata path

--- code/recollect.R
+++ code/recollect.R
@@ -16,7 +16,7 @@ bks_plato <- bind_rows(
 
 
 # load the Rdata file containing the original number of rows
-load(file.path(processed, "data/bks-full-misc-data.Rda")) # opens `n_rides`
+load(file.path(processed, "data/bks-full-misc-data.Rdata")) # opens `n_rides`
 
 # if the number of rows hasn't changed from the original, drop original and components
 if (assert_that(n_rides == nrow(bks_plato))) {
