18ba16659b40f0b9fbac684f52300495be2e9748
Tom <buscandoaverroes@icloud.com>
2022-04-07 20:53:04 +0900

    working station info

--- code/sandbox-json.R
+++ code/sandbox-json.R
@@ -1,2 +1,10 @@
 # sandbox-json.R
 # a sandbox for accessing system data from https://gbfs.capitalbikeshare.com/gbfs/gbfs.json
+library(jsonlite)
+library(dplyr)
+library(RCurl)
+
+url <- "https://gbfs.capitalbikeshare.com/gbfs/en/station_information.json"
+json <- fromJSON(url)
+
+stations <- json$data$stations
